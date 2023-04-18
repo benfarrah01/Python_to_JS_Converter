1cad2f67342144f1079dbb6cc8efd711dfcdb129
bergasanargya <anargya01@allegheny.edu>
2023-04-18 17:14:52 -0400

    Add some functionality to GenerateAST

--- src/GenerateAST.py
+++ src/GenerateAST.py
@@ -2,6 +2,21 @@ import ast
 import sys
 # from rich.console import Console
 
+operator_map = {
+    ast.Add: '+',
+    ast.Sub: '-',
+    ast.Mult: '*',
+    ast.Div: '/',
+    ast.Mod: '%',
+    ast.Pow: '**',
+    ast.BitAnd: '&',
+    ast.BitOr: '|',
+    ast.BitXor: '^',
+    ast.LShift: '<<',
+    ast.RShift: '>>',
+    ast.FloorDiv: '//'
+}
+
 def node_translation(node):
     if isinstance(node, ast.Module):
         return {'type': 'Program', 'body': [node_translation(child) for child in node.body]} 
@@ -15,10 +30,28 @@ def node_translation(node):
         return{'type': 'CallExpression', 'callee': node_translation(node.func), 'arguments': [node_translation(arg) for arg in node.args]}
     elif isinstance(node, ast.Name):
         return {'type': 'Identifier', 'name': node.id}
+    elif isinstance(node, ast.arg):
+        return {'type': 'Identifier', 'name': node.arg}
+    elif isinstance(node, ast.JoinedStr):
+        elements = []
+        for n in node.values:
+            if isinstance(node, ast.Str):
+                elements.append({'type': 'Literal', 'value': n.s})
+            elif isinstance(node, ast.FormattedValue):
+                elements.append(node_translation(n))
+        return {'type': 'BinaryExpression', 'operator': '+', 'left': node_translation(node.values[0]), 'right': node_translation(node.values[1]) if len(elements) > 1 else None}
     elif isinstance(node, ast.Str):
         return {'type': 'Literal', 'value': node.s}
     elif isinstance(node, ast.Num):
-        return {'type': 'Identifier', 'name': node.n}
+        return {'type': 'Identifier', 'value': node.n}
+    elif isinstance(node, ast.Constant):
+        return {'type': 'Literal', 'value': node.value}
+    elif isinstance(node, ast.BoolOp):
+        operator_op = {ast.And: '&&', ast.Or: '||'}
+        operator = operator_op[node.op.__class__]
+        return {'type': 'LogicalExpression', 'operator': operator, 'left': node_translation(node.values[0]), 'right': node_translation(node.values[1])}
+    elif isinstance(node, ast.BinOp):
+        return {'type': 'BinaryExpression', 'operator': operator_map[node.op.__class__], 'left': node_translation(node.left), 'right': node_translation(node.right)}
     else:
         raise Exception(f'Unsupported node type: {type(node).__name__}')
     
