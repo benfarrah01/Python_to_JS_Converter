9b5e330ce550609601b944426bf2d23007c24c4b
Daniel Bekele <bekele01@cslaptop.localdomain>
2023-04-23 18:17:30 -0400

    jsAst to Js code

--- src/main.py
+++ src/main.py
@@ -19,3 +19,24 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+def generate_code(node):
+    if node['type'] == 'Program':
+        return generate_code(node['body'])
+    elif node['type'] == 'BlockStatement':
+        return '{\n' + '\n'.join([generate_code(child) for child in node['body']]) + '\n}'
+    elif node['type'] == 'VariableDeclaration':
+        return f'{node["kind"]} {generate_code(node["declarations"][0])};'
+    elif node['type'] == 'VariableDeclarator':
+        return f'{node["id"]["name"]} = {generate_code(node["init"])}'
+    elif node['type'] == 'Literal':
+        return str(node['value'])
+    elif node['type'] == 'CallExpression':
+        args = ', '.join([generate_code(arg) for arg in node['arguments']])
+        return f'{generate_code(node["callee"])}({args})'
+    elif node['type'] == 'Identifier':
+        return node['name']
+    else:
+        raise NotImplementedError(f'Unsupported node type: {node["type"]}')
+
