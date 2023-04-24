746d4a5190a3ae303ab2b2f335c9abe28c580d20
Daniel Bekele <bekele01@cslaptop.localdomain>
2023-04-24 14:49:52 -0400

    potential ast to code translator

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import sys
 import ast
-import escodegen 
+import escodegen
 
 # import scanning
 import GenerateAST
@@ -35,8 +35,40 @@ ast_node = {
     ],
 }
 
-def generate_code(code):
-    return escodegen.generate(code)
+"""
+def translate(node):
+    if "type" not in node.keys():
+        return ""
+    elif node["type"] == "Identifier":
+        return node["name"]
+    elif node["type"] == "Literal":
+        return str(node["value"])
+    elif node["type"] == "BinaryExpression":
+        return (
+            f"({translate(node['left'])} {node['operator']} {translate(node['right'])})"
+        )
+    elif node["type"] == "UnaryExpression":
+        return f"({node['operator']} {translate(node['argument'])})"
+    elif node["type"] == "BlockStatement":
+        return "\n".join([translate(child) for child in node["body"]])
+    elif node["type"] == "WhileStatement":
+        return f"while ({translate(node['test'])}) {{\n{translate(node['body'])}\n}}"
+    elif node["type"] == "IfStatement":
+        return f"if ({translate(node['test'])}) {{\n{translate(node['consequent'])}\n}}{'else {'+translate(node['alternate'])+'}' if node['alternate'] else ''}"
+    elif node["type"] == "BreakStatement":
+        return "break;"
+    elif node["type"] == "AssignmentExpression":
+        return f"{translate(node['left'])} = {translate(node['right'])}"
+    elif node["type"] == "ForStatement":
+        init = translate(node["init"]) if "init" in node else ""
+        test = translate(node["test"]) if "test" in node else ""
+        update = translate(node["update"]) if "update" in node else ""
+        return f"for ({init}; {test}; {update}) {{\n{translate(node['body'])}\n}}"
+    elif node["type"] == "ArrayExpression":
+        return f"[{', '.join([translate(child) for child in node['elements']])}]"
+    else:
+        return ""
+"""
 
 
 def main():
@@ -46,7 +78,7 @@ def main():
         python_text = python_path.read_text().splitlines()
         tree = ast.parse(python_path.read_text())
         js_ast = GenerateAST.node_translation(tree)
-        final_code = generate_code(js_ast)
+        final_code = translate(js_ast)
         print(final_code)
         # print(python_text)
         # my_scanner = scanning.scanner(python_text)
