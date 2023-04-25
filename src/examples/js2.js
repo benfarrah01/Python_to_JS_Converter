27d2224b258886ca3915bb2ca94f0397f315c1e4
benfarrah01 <farrah01@allegheny.edu>
2023-04-25 13:48:49 -0400

    Update js2.js output and add comments to generate_code

--- src/examples/js2.js
+++ src/examples/js2.js
@@ -1,5 +1,6 @@
 var my_list = [1, 2, 3, 4];
+var new_list = [];
 for (let i = 0; i < my_list.length; i++) {
-    my_list[i] = my_list[i] + 1;
+    new_list[i] = my_list[i] + 1;
 }
 console.log(my_list);
--- src/generate_code.py
+++ src/generate_code.py
@@ -1,54 +1,25 @@
-variables = []
-js = {
-    "variables": {
-        "names": [],
-        "values": []
-    },
-    "expressions": {
-    }
-
-}
+# Empty list to store translated code
+    # Each element is a string representation of a line of code
 code = []
 
 def generate(tree):
-
+    # Iterate through each tree to get expressions
     for key in tree:
         for expr in tree[key]:
-            print(expr)
+            # Print expression for testing purposes, DELETE THIS LINE LATER
+            if type(expr) == str:
+                pass
+            else:
+                print(expr)
             print()
+            # Match each expression with its expression type, handle accordingly
             find_expression(expr)
 
-    # create variable initialization line
-    names = js["variables"]["names"]
     print()
+    # Print each line of translated JavaScript code
     for c in code:
         print(c)
-    # print(names)
-    # for i in range(len(names)-1):
-    #     code[0] = code[0]+(names[i]+", ")
-    # code[0] = code[0]+(names[i+1]+";")
-    # print(code)
-    # print(js["variables"]["values"])
 
-    # # give variables values
-    # i = 0
-    # values = js["variables"]["values"]
-    # for i in range(len(names)-1):
-    #     code.append(f"{names[i]} = {values[i]}")
-    # print()
-    # for c in code:
-    #     print(c)
-
-# def get_variables(tree):
-#     # find variables, put in list
-#     for key in tree:
-#         for expr in tree[key]:
-#             print(expr)
-#             print()
-#             if type(expr) == str:
-#                 pass
-#             else:
-#                 names_vals(expr)
 
 def find_expression(expr):
     if type(expr) == str:
@@ -60,46 +31,60 @@ def find_expression(expr):
     elif expr['type'] == 'ForStatement':
         ForStatement(expr)
 
+
 def AssignmentExpression(expr):
     # Variable to control scoping. Default is function-scoped
     scope = "var"
+
+    # code left of operator
     left = ""
+    # code right of operator
     right = ""
+
     # Error handling, because there might not be a previous line in the code
     try:
         # Use block-scoping if the previous line sets up a new block
+            # note: IN PROGRESS. Need to make it detect if it's in a block after the first line of a for loop
         if "for" in code[-1]:
             scope = "let"
     except IndexError:
         pass
 
-    # check if the AssignmentExpression is an Identifier
-        # return a line of JavaScript code
-    
-
+    # Check if left side is MemberExpression
     if expr['left']['type'] == 'MemberExpression':
+        # ("    name[property]")
         left = (f"\t{expr['left']['object']['name']}[{expr['left']['property']['name']}]")
+
+        # If left is MemberExpression and right is BinaryExpression
         if expr['right']['type'] == 'BinaryExpression':
+            # Get the BinaryExpression's operator
             operator = expr['right']['operator']
+            # ("name2[property] operator [value];")
             right = (f"{expr['right']['left']['object']['name']}[{expr['left']['property']['name']}] {operator} {expr['right']['right']['value']};")
+
+            # Get the entire expression's operator (usually "=")
             operator = expr['operator']
+
+            # Connect the parts to get a finished line of code
             code.append(f"{left} {operator} {right}")
             return
 
-
+   # Check if left side is Identifier
     elif expr['left']['type'] == 'Identifier':
+        # ("scope name")
         left = (f"{scope} {expr['left']['name']}")
 
-
+    # Check if right side is Identifier
     if expr['right']['type'] == 'Identifier':
+        # ("value;")
         right = (f"{expr['right']['value']};")
     
-    # BinaryExpression
+    # Check if right side BinaryExpression
     elif expr['right']['type'] == 'BinaryExpression':
         operator = expr['right']['operator']
         right = (f"{expr['right']['left']['name']} {operator} {expr['right']['right']['name']};")
     
-    # ArrayExpression
+    # Check if right side ArrayExpression
     elif expr['right']['type'] == 'ArrayExpression':
         elements = []
         for i in expr['right']['elements']:
