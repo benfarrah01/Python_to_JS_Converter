723ead1f33160ec39028d73181686fd5f8baf72b
benfarrah01 <farrah01@allegheny.edu>
2023-04-25 10:14:27 -0400

    Add comments to main

--- src/main.py
+++ src/main.py
@@ -1,8 +1,5 @@
 import sys
 import ast
-#import escodegen
-
-# import scanning
 import generate_ast
 import generate_code
 from pathlib import Path
@@ -39,12 +36,22 @@ ast_node = {
 
 
 def main():
+    # Get argument from command
+        # arg is the path of the Python file to be translated
     python_file = sys.argv[1]
+
+    # Convert argument to path variable
     python_path = Path(python_file)
+
+    # Check if the path leads to a file
     if python_path.is_file():
-        python_text = python_path.read_text().splitlines()
+        # Read the text from the file, store in string. Parse the string to get a Module object (from AST).
         tree = ast.parse(python_path.read_text())
+
+        # Pass tree into node_translation() to get AST
         js_ast = generate_ast.node_translation(tree)
+
+        # Pass AST into generate() to get translated JavaScript code
         generate_code.generate(js_ast)
 
 
