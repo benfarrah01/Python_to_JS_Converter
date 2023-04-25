95b8cfbbb4d8c0da8cb98fa09ae991c00af57904
benfarrah01 <farrah01@allegheny.edu>
2023-04-25 09:47:44 -0400

    Move unused files into folder, create test suite

--- src/main.py
+++ src/main.py
@@ -36,40 +36,6 @@ ast_node = {
     ],
 }
 
-"""
-def translate(node):
-    if "type" not in node.keys():
-        return ""
-    elif node["type"] == "Identifier":
-        return node["name"]
-    elif node["type"] == "Literal":
-        return str(node["value"])
-    elif node["type"] == "BinaryExpression":
-        return (
-            f"({translate(node['left'])} {node['operator']} {translate(node['right'])})"
-        )
-    elif node["type"] == "UnaryExpression":
-        return f"({node['operator']} {translate(node['argument'])})"
-    elif node["type"] == "BlockStatement":
-        return "\n".join([translate(child) for child in node["body"]])
-    elif node["type"] == "WhileStatement":
-        return f"while ({translate(node['test'])}) {{\n{translate(node['body'])}\n}}"
-    elif node["type"] == "IfStatement":
-        return f"if ({translate(node['test'])}) {{\n{translate(node['consequent'])}\n}}{'else {'+translate(node['alternate'])+'}' if node['alternate'] else ''}"
-    elif node["type"] == "BreakStatement":
-        return "break;"
-    elif node["type"] == "AssignmentExpression":
-        return f"{translate(node['left'])} = {translate(node['right'])}"
-    elif node["type"] == "ForStatement":
-        init = translate(node["init"]) if "init" in node else ""
-        test = translate(node["test"]) if "test" in node else ""
-        update = translate(node["update"]) if "update" in node else ""
-        return f"for ({init}; {test}; {update}) {{\n{translate(node['body'])}\n}}"
-    elif node["type"] == "ArrayExpression":
-        return f"[{', '.join([translate(child) for child in node['elements']])}]"
-    else:
-        return ""
-"""
 
 
 def main():
