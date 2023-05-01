db11983ff4b5fe3addb5035cf36649d5f5b9d669
bergasanargya <anargya01@allegheny.edu>
2023-05-01 17:29:46 -0400

    Added first test for generate_ast

--- src/test_suite/test_generate_ast.py
+++ src/test_suite/test_generate_ast.py
@@ -1,2 +1,18 @@
 import pytest
-import generate_ast
+import ast
+from generate_ast import node_translation
+
+def test_moduletranslation():
+    node = ast.parse("x = 1")
+    expected_output = {
+    "type": "Program",
+    "body": [
+        {
+            "type": "AssignmentExpression",
+            "operator": "=",
+            "left": {"type": "Identifier", "name": "x"},
+            "right": {"type": "Identifier", "value": 1}
+        }
+    ],
+}
+    assert node_translation(node) == expected_output
