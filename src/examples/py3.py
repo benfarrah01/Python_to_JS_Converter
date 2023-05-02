bb3c779f19d574bec338380e65c641eff0ff7e64
benfarrah01 <farrah01@allegheny.edu>
2023-05-02 01:11:03 -0400

    Add final test case

---
+++ src/examples/py3.py
@@ -0,0 +1,200 @@
+#These variables are numbers
+num1 = 1
+num2 = 2
+num3 = 3
+num4 = 4
+num5 = 5
+#These variables are being displayed
+print(num1)
+print(num2)
+print(num3)
+print(num4)
+print(num5)
+#These variables are lists
+list1 = [1,2,3,4,5]
+list2 = [6,7,8,9,10]
+list3 = [11,12,13,14,15]
+list4 = [16,17,18,19,20]
+list5 = [21,22,23,24,25]
+#For loops
+for i in list1:
+    list1[i] = list1[i] + 5
+num1 = 2
+for i in list2:
+    list2[i] = list2[i] + 5
+num2 = 3
+for i in list3:
+    list3[i] = list3[i] + 5
+num3 = 4
+for i in list4:
+    list4[i] = list4[i] + 5
+num4 = 5
+for i in list5:
+    list5[i] = list5[i] + 5
+num5 = 1
+num6 = 6
+num7 = 7
+num8 = 8
+num9 = 9
+num10 = 10
+print(num6)
+print(num7)
+print(num8)
+print(num9)
+print(num10)
+list6 = [6,7,8,9,10]
+list7 = [11,12,13,14,15]
+list8 = [16,17,18,19,20]
+list9 = [21,22,23,24,25]
+list10 = [1,2,3,4,5]
+for i in list6:
+    list6[i] = list6[i] + 6
+num6 = 3
+for i in list7:
+    list7[i] = list7[i] + 6
+num7 = 4
+for i in list8:
+    list8[i] = list8[i] + 6
+num8 = 5
+for i in list9:
+    list9[i] = list9[i] + 6
+num9 = 1
+for i in list10:
+    list10[i] = list10[i] + 6
+num10 = 2
+num11 = 11
+num12 = 12
+num13 = 13
+num14 = 14
+num15 = 15
+print(num11)
+print(num12)
+print(num13)
+print(num14)
+print(num15)
+list11 = [11,12,13,14,15]
+list12 = [16,17,18,19,20]
+list13 = [21,22,23,24,25]
+list14 = [1,2,3,4,5]
+list15 = [6,7,8,9,10]
+for i in list11:
+    list11[i] = list11[i] + 7
+num11 = 4
+for i in list12:
+    list12[i] = list12[i] + 7
+num12 = 5
+for i in list13:
+    list13[i] = list13[i] + 7
+num13 = 1
+for i in list14:
+    list14[i] = list14[i] + 7
+num14 = 2
+for i in list15:
+    list15[i] = list15[i] + 7
+num15 = 3
+num16 = 16
+num17 = 17
+num18 = 18
+num19 = 19
+num20 = 20
+print(num16)
+print(num17)
+print(num18)
+print(num19)
+print(num20)
+list16 = [16,17,18,19,20]
+list17 = [21,22,23,24,25]
+list18 = [1,2,3,4,5]
+list19 = [6,7,8,9,10]
+list20 = [11,12,13,14,15]
+for i in list16:
+    list16[i] = list16[i] + 8
+num16 = 5
+for i in list17:
+    list17[i] = list17[i] + 8
+num17 = 1
+for i in list18:
+    list18[i] = list18[i] + 8
+num18 = 2
+num21 = 21
+num22 = 22
+num23 = 23
+num24 = 24
+num25 = 25
+print(num21)
+print(num22)
+print(num23)
+print(num24)
+print(num25)
+list21 = [21,22,23,24,25]
+list22 = [1,2,3,4,5]
+list23 = [6,7,8,9,10]
+list24 = [11,12,13,14,15]
+list25 = [16,17,18,19,20]
+for i in list21:
+    list21[i] = list21[i] + 9
+num21 = 5
+for i in list22:
+    list22[i] = list22[i] + 9
+num22 = 1
+for i in list23:
+    list23[i] = list23[i] + 9
+num23 = 2
+for i in list24:
+    list24[i] = list24[i] + 9
+num24 = 3
+for i in list25:
+    list25[i] = list25[i] + 9
+num25 = 4
+num26 = 26
+num27 = 27
+num28 = 28
+num29 = 29
+num30 = 30
+print(num26)
+print(num27)
+print(num28)
+print(num29)
+print(num30)
+list26 = [26,27,28,29,30]
+list27 = [16,17,18,19,20]
+list28 = [21,22,23,24,25]
+list29 = [1,2,3,4,5]
+list30 = [6,7,8,9,10]
+for i in list26:
+    list26[i] = list26[i] + 10
+num26 = 1
+for i in list27:
+    list27[i] = list27[i] + 10
+num27 = 2
+for i in list28:
+    list28[i] = list28[i] + 10
+num28 = 3
+for i in list29:
+    list29[i] = list29[i] + 10
+num29 = 4
+for i in list30:
+    list30[i] = list30[i] + 10
+num30 = 5
+num31 = 31
+num32 = 32
+num33 = 33
+num34 = 34
+num35 = 35
+print(num31)
+print(num32)
+print(num33)
+print(num34)
+print(num35)
+list31 = [31,32,33,34,35]
+list32 = [11,12,13,14,15]
+list33 = [16,17,18,19,20]
+list34 = [21,22,23,24,25]
+list35 = [1,2,3,4,5]
+for i in list31:
+    list31[i] = list31[i] + 11
+num31 = 2
+for i in list32:
+    list32[i] = list32[i] + 11
+celebrationnumber = 200
+print(celebrationnumber)
